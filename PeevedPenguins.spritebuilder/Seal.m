e9bc79c557596f6937118d3f54186833f9435bd5
Robert Eastmond <roberteastmond@Roberts-MacBook-Pro-2.local>
2014-06-28 16:35:13 -0600

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Seal.m
@@ -0,0 +1,21 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Robert Eastmond on 6/28/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+-(id)init{
+    self = [super init];
+    
+    if(self){
+        CCLOG(@"Seal Created");
+    }
+    return self;
+}
+
+@end
