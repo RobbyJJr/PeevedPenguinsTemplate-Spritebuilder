a6892765572431982c80ac3675b1fc0d2e107f7c
Robert Eastmond <roberteastmond@Roberts-MacBook-Pro-2.local>
2014-06-30 11:47:37 -0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -146,7 +146,22 @@
     
 }
 
--(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB{
-    CCLOG(@"Something collided with a seal");
+- (void)sealRemoved:(CCNode *)seal {
+    [seal removeFromParent];
 }
+
+-(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB{
+    //CCLOG(@"Something collided with a seal");
+    
+    float energy = [pair totalKineticEnergy];
+    
+    //if energy is large enough, remove the seal
+    if (energy > 5000.f) {
+        [[_physicsNode space] addPostStepBlock:^{
+            [self sealRemoved:nodeA];
+        }key:nodeA];
+    }
+}
+
+
 @end
